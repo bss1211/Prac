757aa302334a7a7baa5a795f509edb22d9bd5aa4
B <91157753+bss1211@users.noreply.github.com>
2022-10-30 13:10:11 +0530

    Add files via upload

---
+++ spyder prac.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 20 13:26:55 2022
+
+@author: Aragog
+"""
+def sum(a, b):
+     return (a+b)
+     
+a = int(input('Enter number:'))
+b = int(input('Enter number b value : '))
+ 
+print(f'Sum of{a} and {b} is {sum(a,b)}')
+ 
+ 
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+#def sum(a, b):
+    #3return (a + b)
+
+#a = int(input('Enter 1st number: '))
+#b = int(input('Enter 2nd number: '))
+
+#print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+
+val = input("Enter your value: ")
+print(val)
+
+name = input('What is your name?\n')     
+ print(name)
